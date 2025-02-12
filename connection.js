0321d16f534f78f989c7167dfafeec5290b808ad
Marcos Vitor <70297708+marcosvitor1994@users.noreply.github.com>
2025-02-12 09:19:13 -0300

    Update connection.js
    adição rota /clientes_adriel

--- connection.js
+++ connection.js
@@ -122,6 +122,18 @@ app.get('/getDataEngInventario', async (req, res) => {
   }
 });
 
+app.get('/clientes_adriel', async (req, res) => {
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: '18qQpZbOuMIR3RieJ7TxM8z4xveUZ9R3jEUsWLCVAz7Q',
+      range: 'Dados_Consolidados',
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar os dados do Google Sheets', details: error.message });
+  }
+});
+
 
 
 
