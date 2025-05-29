38942556ed137bbb71f5a3c05c0aff69c1d377d3
Marcos Vitor <70297708+marcosvitor1994@users.noreply.github.com>
2025-05-29 10:06:16 -0300

    Update /ccbbMeta

--- connection.js
+++ connection.js
@@ -218,6 +218,18 @@ app.get('/ShareCcbb', async (req, res) => {
   }
 });
 
+app.get('/ccbbMeta', async (req, res) => {
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: '1O_aS1D1x_BQFjyiApz1HoRO6eAxIs5rnrHE84A23aHs',
+      range: 'Meta - Base de Criativos',
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar os dados do Google Sheets', details: error.message });
+  }
+});
+
 app.get('/', (req, res) => {
   res.status(200).json({
     status: "Success",
