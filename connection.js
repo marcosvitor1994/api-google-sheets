13050f3074119d162096c8fc059d708577d7816b
Marcos Vitor <70297708+marcosvitor1994@users.noreply.github.com>
2025-06-17 13:12:58 -0300

    Update /ShareCcbb

--- connection.js
+++ connection.js
@@ -210,7 +210,7 @@ app.get('/ShareCcbb', async (req, res) => {
   try {
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: '1O_aS1D1x_BQFjyiApz1HoRO6eAxIs5rnrHE84A23aHs',
-      range: 'Share de internet',
+      range: 'Share de internet Marcos Vitor',
     });
     res.json(response.data);
   } catch (error) {
